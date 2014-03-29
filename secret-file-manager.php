d67829e6e7bd1fbf5dca59c6c9fcf22786b404cf
Jakub Klapka <klapka@lumiart.cz>
2014-03-29 17:16:14 +0100

    Initial commit

---
+++ secret-file-manager.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Secret File Manager
+ * Description: Upload big files via FTP and create secret link to send them to clients
+ * Version: 0.9
+ * Author: Jakub Klapka (Lumiart.cz)
+ * Author URI: http://www.lumiart.cz
+ */
+
